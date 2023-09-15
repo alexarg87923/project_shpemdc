58f329f36863bd2387790e316d89f4f6c4206079
Alex Arguelles <alexarguelles87923@gmail.com>
2023-09-14 23:19:08 -0400

    updating

--- static/assets/js/modalscript.js
+++ static/assets/js/modalscript.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
-    $("#showModal").click(function(){
+    $(".showModal").click(function(){
         $("#myModal").modal('show');
     });
     
     $("#showPopup").click(function(){
         $("#myPopup").toggleClass('d-none');
     });
-});
-$(document).ready(function(){
+    
     $("#closeModal").click(function(){
         $("#myModal").modal('hide');
     });
